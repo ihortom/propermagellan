ffdcc76f71ddb7169c7d0cd3e63699cd827fabc7
ihortom <inet@tomilenko.tk>
2015-11-20 00:20:05 -0600

    Adjusted menu behaviour on windows scroll and resize.

---
+++ README.md
@@ -0,0 +1,15 @@
+## ProperMagellan
+### A Responsive WordPress theme based on "Foundation" front-end framework
+
+An attempt to create a responsive WordPress theme for a simple one-page web site
+ with a fixed menu bar with the menu items referencing various sections of the page. 
+Each section, in fact, would be an individual WordPress page.
+
+The theme is to serve as a framework to more advanced and customised themes for 
+one-page designed web sites.
+
+Depends on [**Foundation**](http://foundation.zurb.com/) CSS/Javascript framework by Zurb.
+
+Uses [**Sass**](http://sass-lang.com/) to generate and compile custom style sheets.
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,37 @@
 jQuery(document).foundation();
+
+jQuery(document).ready(function($) {
+    
+    var adjustMenu = function() {        
+        var isMagellan = ($('[data-magellan-expedition-clone]').length > 0) ? true : false;
+        var isSmall = ($(window).width() <= 640) ? true : false;
+        var topBar = $('[data-magellan-expedition] .top-bar-section');
+        var titleArea = $('[data-magellan-expedition] .title-area .name');
+        if (isSmall) {
+            titleArea.show();
+            if (topBar.hasClass('right')) { 
+                topBar.removeClass('right');
+            }
+        }
+        else {
+            if (isMagellan) {
+                titleArea.show();
+                topBar.addClass('right');
+            }
+            else {
+                titleArea.hide();
+                topBar.removeClass('right');
+            }
+        }
+    }
+    
+    adjustMenu();
+    $(document).scroll(function() {        
+        //$(document).foundation('magellan-expedition', 'reflow');
+        adjustMenu();
+    });
+    $(window).resize(function() {
+        //$(document).foundation('magellan-expedition', 'reflow');
+        adjustMenu();
+    });
+});
